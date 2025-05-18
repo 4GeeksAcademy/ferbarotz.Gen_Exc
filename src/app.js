a8f331e1d6351359f59c9c1b9ecdef08de946223
Ferbarotz <ferbarotz23@gmail.com>
2025-05-18 17:10:31 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -6,20 +6,21 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
-let who = ['Mi Esposa', 'Mi Madre', 'Mi Padre', 'Mi Hermana'];
-let action = ['vendio', 'regalo', 'presto', 'daño'];
-let what = ['la moto', 'el carro', 'el despertador' ,'el televisor'];
-let when = ['ayer', 'hoy', 'en la mañana', 'en la noche'];
+  let who = ['Mi Esposa', 'Mi Madre', 'Mi Padre', 'Mi Hermana'];
+  let action = ['vendio', 'regalo', 'presto', 'daño'];
+  let what = ['la moto', 'el carro', 'el despertador', 'el televisor'];
+  let when = ['ayer', 'hoy', 'en la mañana', 'en la noche'];
 
-function generadorDeExcusas(who, action, what, when) {
-  let randomwho = Math.floor(Math.random() * who.length);
-  let randomaction = Math.floor(Math.random() * action.length);
-  let randomwhat = Math.floor(Math.random() * what.length);
-  let randomwhen = Math.floor(Math.random() * when.length);
-return (who[randomwho] + " " + action[randomaction]+ " "  + what[randomwhat]+ " "  +  when[randomwhen]);
+  function orderLet(option) {
+    return option[Math.floor(Math.random() * option.length)];
+  }
+
+  function generadorDeExcusas() {
+    return orderLet(who) + " " + orderLet(action) + " " + orderLet(what) + " " + orderLet(when);
+  }
+
+  console.log(generadorDeExcusas(who, action, what, when));
+  document.querySelector("#excuse").innerHTML = generadorDeExcusas(who, action, what, when);
 }
-  console.log( generadorDeExcusas(who, action, what, when));
-  document.querySelector("#excuse").innerHTML=generadorDeExcusas(who, action, what, when);
-};
